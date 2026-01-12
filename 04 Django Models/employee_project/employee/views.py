b0e4dbcd892dbf43d8591cb282a88b58204db569
Rajesh <rajarajesha@gmail.com>
2026-01-12 10:56:37 +0530

    'djangomodels'

---
+++ 04 Django Models/employee_project/employee/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Employee
+# Create your views here.
+def employeeview(request):
+    emp_data = Employee.objects.all()
+    my_dict = {'emp_data':emp_data}
+    return render(request, 'employee/home.html', context=my_dict)
