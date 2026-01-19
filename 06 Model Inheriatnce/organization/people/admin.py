34847fe2842ee83b0aadca3dbb1c319e1cfa2262
Rajesh <rajarajesha@gmail.com>
2026-01-19 11:09:40 +0530

    'minheritance'

---
+++ 06 Model Inheriatnce/organization/people/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Employee, EmployeeProxy, Customer, CustomerProxy
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(EmployeeProxy)
+admin.site.register(Customer)
+admin.site.register(CustomerProxy)
